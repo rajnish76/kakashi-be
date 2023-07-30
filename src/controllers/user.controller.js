3ed755ff7bfec8274bc677ef8f5f0d305983f286
Rajnish Srivastava <rajnishsrivastava@Rajnishs-MacBook-Air.local>
2023-07-30 15:08:00 +0530

    user api

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,8 +3,12 @@ const catchAsync = require('../utils/catchAsync');
 const { User } = require('../models');
 
 const register = catchAsync(async (req, res) => {
-  const user = await User.create(req.body);
-  res.status(httpStatus.CREATED).send({ user });
+  let user = await User.find({ userId: req.params.id });
+  if (!user) {
+    user = await User.create(req.body);
+    return res.status(httpStatus.CREATED).send({ s: true, user });
+  }
+  return res.send({ s: true, user });
 });
 
 module.exports = {
--- src/routes/v1/user.route.js
+++ src/routes/v1/user.route.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const validate = require('../../middlewares/validate');
-const userValidation = require('../../validations/user.validation');
+// const validate = require('../../middlewares/validate');
+// const userValidation = require('../../validations/user.validation');
 const userController = require('../../controllers/user.controller');
 
 const router = express.Router();
 
-router.post('/register', validate(userValidation.registerUser), userController.register);
+router.post('/register', userController.register);
 
 module.exports = router;
