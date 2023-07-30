d45d20ffaabd87d55590443ca042c7eb8bcbc2b3
Rajnish Srivastava <rajnishsrivastava@Rajnishs-MacBook-Air.local>
2023-07-30 16:07:11 +0530

    added data

--- src/models/category.model.js
+++ src/models/category.model.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const slug = require('mongoose-slug-generator');
+// const slug = require('mongoose-slug-generator');
 const { toJSON, paginate } = require('./plugins');
 
 const categorySchema = mongoose.Schema(
@@ -9,11 +9,11 @@ const categorySchema = mongoose.Schema(
       trim: true,
       unique: true,
     },
-    slug: {
-      type: String,
-      slug: 'name',
-      unique: true,
-    },
+    // slug: {
+    //   type: String,
+    //   slug: 'name',
+    //   unique: true,
+    // },
     image: {
       type: String,
     },
@@ -40,11 +40,11 @@ const categorySchema = mongoose.Schema(
 // add plugin that converts mongoose to json
 categorySchema.plugin(toJSON);
 categorySchema.plugin(paginate);
-categorySchema.plugin(slug, {
-  separator: '-',
-  lang: 'en',
-  truncate: 120,
-});
+// categorySchema.plugin(slug, {
+//   separator: '-',
+//   lang: 'en',
+//   truncate: 120,
+// });
 
 /**
  * @typedef Category
--- src/models/plugins/paginate.plugin.js
+++ src/models/plugins/paginate.plugin.js
@@ -36,7 +36,7 @@ const paginate = (schema) => {
     const page = options.page && parseInt(options.page, 10) > 0 ? parseInt(options.page, 10) : 1;
     const skip = (page - 1) * limit;
 
-    const countPromise = this.countDocuments(filter).exec();
+    const countPromise = await this.countDocuments(filter);
     let docsPromise = this.find(filter).sort(sort).skip(skip).limit(limit);
 
     if (options.populate) {
@@ -50,20 +50,18 @@ const paginate = (schema) => {
       });
     }
 
-    docsPromise = docsPromise.exec();
+    const [totalResults, results] = await Promise.all([countPromise, docsPromise]);
 
-    return Promise.all([countPromise, docsPromise]).then((values) => {
-      const [totalResults, results] = values;
-      const totalPages = Math.ceil(totalResults / limit);
-      const result = {
-        results,
-        page,
-        limit,
-        totalPages,
-        totalResults,
-      };
-      return Promise.resolve(result);
-    });
+    const totalPages = Math.ceil(totalResults / limit);
+    const result = {
+      results,
+      page,
+      limit,
+      totalPages,
+      totalResults,
+    };
+
+    return result;
   };
 };
 
